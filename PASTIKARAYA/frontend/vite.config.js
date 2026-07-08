503795632f7ade46baf58b17329461b8fa227e92
Bonaasm <portoakun1@gmail.com>
2026-07-08 14:54:05 +0700

    Update vite.config.js

--- PASTIKARAYA/frontend/vite.config.js
+++ PASTIKARAYA/frontend/vite.config.js
@@ -5,9 +5,7 @@ export default defineConfig({
   plugins: [react()],
   preview: {
     host: "0.0.0.0",
-    port: process.env.PORT,
-    allowedHosts: [
-      "pastika-production-3688.up.railway.app",
-    ],
+    port: Number(process.env.PORT) || 4173,
+    allowedHosts: true,
   },
 });
