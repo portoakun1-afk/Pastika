b77e19a94a4a4abe011015b1da628ca56c5b23e4
portoakun1-afk <portoakun1@gmail.com>
2026-07-16 00:49:12 +0700

    Update Layout.jsx

--- PASTIKARAYA/frontend/src/components/Layout.jsx
+++ PASTIKARAYA/frontend/src/components/Layout.jsx
@@ -2,21 +2,25 @@ import Sidebar from "./Sidebar";
 
 function Layout({ children }) {
   return (
-    <div className="d-flex">
+    <div className="container-fluid p-0">
+      <div className="row g-0">
 
-      <Sidebar />
+        <div className="col-12 col-md-auto">
+          <Sidebar />
+        </div>
 
-      <div
-        className="flex-grow-1"
-        style={{
-          background: "#f4f6f9",
-          minHeight: "100vh",
-          padding: "30px",
-        }}
-      >
-        {children}
+        <div
+          className="col"
+          style={{
+            background: "#f4f6f9",
+            minHeight: "100vh",
+            padding: "20px",
+          }}
+        >
+          {children}
+        </div>
+
       </div>
-
     </div>
   );
 }
